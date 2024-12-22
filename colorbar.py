945892882ee9765180fb05ee6c0731915e60d3c8
Eugene <rejgan318@gmail.com>
2024-12-22 19:38:02 +0200

    Initial commit

---
+++ colorbar.py
@@ -0,0 +1,13 @@
+"""
+colorbar - цветной прогресс-бар
+"""
+from time import sleep
+
+
+def colorbar():
+    return "Colorbar"
+
+
+if __name__ == '__main__':
+    print("colorbar demo")
+    print(colorbar())
